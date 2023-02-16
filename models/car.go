c340d8e515de457f34b400fb88b4c98daf431b34
gabriela <gabriela.rudarska@medialaben.no>
2023-02-16 12:31:43 +0200

    Adding migrations and model for car

---
+++ models/car.go
@@ -0,0 +1,15 @@
+package models
+
+type Car struct {
+	Price     string
+	UsageType string
+	Brand     string
+	Model     string
+	Year      string
+	Range     string
+	Type      string
+	MileAge   string
+	Engine    string
+	LogoUrl   string
+	Features  string
+}
